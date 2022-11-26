7c92ed5176a9807c64e63ed6f1aecc6757e960c2
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-11-26 10:14:38 +0600

    features done

--- app/js/app.js
+++ app/js/app.js
@@ -23,3 +23,23 @@ navToggler.addEventListener("click", () => {
         });
     }
 });
+
+// Feature Tab
+const feature = document.querySelector(".feature");
+const buttons = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".feature__content");
+
+feature.addEventListener("click", function (e) {
+    const id = e.target.dataset.id;
+    if (id) {
+        buttons.forEach(function (btn) {
+            btn.classList.remove("active");
+        });
+        e.target.classList.add("active");
+        contents.forEach(function (content) {
+            content.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+});
